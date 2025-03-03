686096434400892cb5fe66823387be6a50b0c28e
Jorge <jrncarrizo@gmail.com>
2025-03-02 21:02:44 -0300

    estoy segundo commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,8 @@
     <title>@yield('title', 'Mi Aplicación')</title>
     <!-- Enlace al CSS de Bootstrap -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Estilos adicionales -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- Estilos y Scripts gestionados por Vite -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     <header>
@@ -19,7 +19,7 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
-                   
+                    <!-- Aquí puedes añadir elementos de navegación -->
                 </ul>
             </div>
         </nav>
@@ -37,7 +37,5 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <!-- Scripts adicionales -->
-    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
